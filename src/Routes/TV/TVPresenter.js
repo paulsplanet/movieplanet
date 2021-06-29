a0b8924210e75bc64621a8dd7cfea342e1ec35e0
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-06-28 17:44:55 -0700

    updating detail Presenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
     padding: 0 20px;
@@ -15,21 +16,42 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => load
         {topRated && topRated.length > 0 && 
             <Section title="Top Rated Shows">
                 {topRated.map(show => (
-                    <span key={show.id}>{show.name}</span>
+                    <Poster 
+                        key={show.id} 
+                        id={show.id} 
+                        title={show.original_name} 
+                        imageUrl={show.poster_path} 
+                        rating={show.vote_average}
+                        year={show.frist_air_date && show.first_air_date.substring(0, 4)} 
+                    />
                 ))}
             </Section>
         }
         {popular && popular.length > 0 && 
             <Section title="Popular Shows">
                 {popular.map(show => (
-                    <span key={show.id}>{show.name}</span>
+                    <Poster 
+                        key={show.id} 
+                        id={show.id} 
+                        title={show.original_name} 
+                        imageUrl={show.poster_path} 
+                        rating={show.vote_average}
+                        year={show.frist_air_date && show.first_air_date.substring(0, 4)} 
+                    />
                 ))}
             </Section>
         }    
         {airingToday && airingToday.length > 0 && 
             <Section title="Airing Today">
                 {airingToday.map(show => (
-                    <span key={show.id}>{show.name}</span>
+                    <Poster 
+                        key={show.id} 
+                        id={show.id} 
+                        title={show.original_name} 
+                        imageUrl={show.poster_path} 
+                        rating={show.vote_average}
+                        year={show.frist_air_date && show.first_air_date.substring(0, 4)} 
+                    />
                 ))}
             </Section>
         } 
