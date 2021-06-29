a780c6636a5599b71496a97bf50106c21a635fd1
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-06-28 19:58:30 -0700

    update detail presenter 2

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -42,12 +42,51 @@ const Cover = styled.div`
     border-radius: 5px;
 `;
 
+const Data = styled.div`
+    width: 65%;
+    margin-left: 10px
+`;
+
+const Title = styled.h3`
+    font-size: 32px;
+`;
+
+const ItemContainer =styled.div`
+    margin: 20px 0;
+`;
+
+const Item = styled.span``;
+
+const Divider = styled.span`
+    margin: 0 10px;
+`;
+
+const Overview = styled.p`
+    font-size: 12px;
+    opacity: 0.7;
+    line-height: 1.5;
+    width: 70%;
+`;
+
 const DetailPresenter = ({ result, loading, error }) => 
     loading ? ( <Loader /> ) : (
         <Container>
             <Backdrop bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`} />
             <Content>
                 <Cover bgImage={result.poster_path ? `https://image.tmdb.org/t/p/original${result.poster_path}` : noPoster}/>
+                <Data>
+                    <Title>{result.original_title ? result.original_title : result.original_name}</Title>
+                    <ItemContainer>
+                        <Item>{result.release_date ? result.release_date.substring(0, 4) : result.first_air_date.substring(0, 4)}</Item>
+                        <Divider>&#07;</Divider>
+                        <Item>{result.runtime ? result.runtime : result.episode_run_time } min</Item>
+                        <Divider>&#07;</Divider>
+                        <Item>{result.genres && 
+                            result.genres.map((genre, index) => index === result.genres.length - 1 ? genre.name : `${genre.name} / `)} 
+                        </Item>
+                    </ItemContainer>
+                    <Overview>{result.overview}</Overview>
+                </Data>
             </Content>
         </Container>
     );
